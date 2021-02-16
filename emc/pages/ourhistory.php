fa8b338612639279a0e47538f0f25a1f876c413d
Eric Rogers <wd20er@emc-lw-erogers.ncep.noaa.gov>
2021-02-16 17:04:02 +0000

    Update about.php

--- emc/pages/ourhistory.php
+++ emc/pages/ourhistory.php
@@ -19,8 +19,10 @@
                                         <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/pages/ourhistory.php">Our History</a></li>
                                         <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/pages/components-leadership.php">Components/Leadership</a></li>
                                         <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/pages/collaborators.php">Collaborators</a></li>
+                                        <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/docs/EMC_Fed_Contractor_Awards.pdf">Awards</a></li>
                                         <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/pages/careers.php">Careers</a></li>
                                         <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/pages/contactus.php">Contact Us</a></li>
+                                        <li class="list-group-item"><a href="https://www.emc.ncep.noaa.gov/emc/pages/staff.php">Staff Directory</a></li>
                                      </ul>
                                  </li>
 							</ul>
