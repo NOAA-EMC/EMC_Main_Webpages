d1db230c1d4e64ccf59cb12eb92cf7d06e08bca1
Khalif Cooper <1598745567113305@mil>
2021-03-16 03:34:55 -0400

    added local forecast query box

--- emc/header.php
+++ emc/header.php
@@ -181,21 +181,11 @@
 							  	<a class="nav-link dropdown-toggle" id="findWeatherIndicator" data-toggle="dropdown" href="#">Find your Local Weather</a>
 							  	<ul class="dropdown-menu">
 							  	<li>
-							  	<form id="submitWeatherForm" class="weather-widget-form no-weather hide-formatting noaa-weather-widget-form"  method="post" accept-charset="UTF-8">
-                                <div class="lower">
-                                  <div class="form-wrapper">
-                                    <div class="form-item form-type-textfield form-item-zip">
-                                      <label class="element-invisible" for="edit-zip">Enter your ZIP code </label><br>
-                                        <input placeholder="Enter City or ZIP code" class="zip-input form-text form-control" name="edit-zip" value="" size="0" maxlength="128" autocomplete="off" type="text">
-    
-                                    </div>
-                                    
-                                    <input name="edit-submit" value="GO" class="form-submit btn btn-primary" type="submit">
-                                  </div>
-                                  <div class="inner"><div id="weather"></div></div>
-<!--  <div class="autocomplete-suggestions" style="position: absolute; display: none; max-height: 300px; z-index: 9999;"></div>-->
-                                </div>
-                          </form>
+								    <form id="local-forecast-form" method="post" action="https://forecast.weather.gov/zipcity.php" target="_blank">
+									<label id="local-forecast-label"><a>Local Forecast</a></label>
+									<input id="local-forecast-input"  id="inputstring" name="inputstring" style="" type="text" size="12" title="Enter ZIP/City, State for local forecast" />
+									<button class="btn btn-primary" id="local-forecast-btn"type="submit" class="btn btn-primary">Go</button>
+									</form>
 							  	</li>
 							  	</ul>
 							  </li>
