11ba145d5c8770ca8ccb3468cdf87acd86e4801f
Eric Rogers <wd20er@emc-lw-erogers.ncep.noaa.gov>
2022-09-02 18:38:41 +0000

    Update GEFS-Aero page with image for Jan 2020 Australian fires, add said image to repo

--- emc/pages/numerical_forecast_systems/gefs_aero-test.php
+++ emc/pages/numerical_forecast_systems/gefs_aero-test.php
@@ -89,7 +89,7 @@ include '../portal_header.php';?>
 
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
+        <h1 class="h2"></h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">
             <!--<button type="button" class="btn btn-sm btn-outline-secondary">Share</button>-->
@@ -97,7 +97,9 @@ include '../portal_header.php';?>
           </div>
         </div>
       </div>
-     <h2 id="section-title">Global Aerosols 5-day Forecasts</h2>
+     <h1 id="section-title"><div style="text-align left; margin-left:440px;">GEFS-Aerosol 5-day Forecasts</h1>
+     <img src="https://www.emc.ncep.noaa.gov/emc/images/GEFS-Aero_Australia_fires.png">
+     <p>
      <img src="../../pages/disclaimer/disclaimer_small.png"  />
      	<div id="description">
     
